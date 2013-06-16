9e2c789ed10a16a51b91fc79fcb4b9bb4bb4da7e
Richo Healey <richo@psych0tik.net>
2013-06-17 00:25:10 +1000

    WIP

--- src/choon.c
+++ src/choon.c
@@ -2,10 +2,11 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
+#include "httppebble-watch/http.c"
 
 #define APP_NAME "choon"
 #define MY_UUID { 0x5A, 0xE7, 0x98, 0x6D, 0x7D, 0x07, 0x4D, 0xCA, 0x85, 0x16, 0x5D, 0xC3, 0xD5, 0x00, 0x5A, 0x77 }
-PBL_APP_INFO(MY_UUID,
+PBL_APP_INFO(HTTP_UUID,
              APP_NAME, "richo@psych0tik.net",
              0, 0, /* App version */
              DEFAULT_MENU_ICON,
@@ -14,13 +15,49 @@ PBL_APP_INFO(MY_UUID,
 Window window;
 
 void select_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
+    /* vibes_short_pulse(); */
+}
 
-    vibes_short_pulse();
+void fire_track_request(char req) {
+    static int ret;
+    static char *url = "http://192.168.43.32/XXXXXXXXXXXX/_";
+    /*                    XXX use choon.io */
+    static DictionaryIterator **body;
+
+    /* url[strlen(url) - 1] = req; */
+    if ((ret = http_out_get(url, 123123123, body)) == HTTP_OK) {
+        http_out_send();
+    } else {
+        switch(ret) {
+            case HTTP_BUSY:
+                vibes_short_pulse();
+            case HTTP_INVALID_ARGS:
+                vibes_short_pulse();
+            case HTTP_NOT_ENOUGH_STORAGE:
+                vibes_short_pulse();
+            case HTTP_INTERNAL_INCONSISTENCY:
+                vibes_short_pulse();
+        }
+    }
+}
+
+void next_track(ClickRecognizerRef recognizer, Window *window) {
+    fire_track_request('n');
 }
 
 void click_config_provider(ClickConfig **config, Window *window) {
     config[BUTTON_ID_SELECT]->click.handler = (ClickHandler) select_single_click_handler;
     config[BUTTON_ID_SELECT]->click.repeat_interval_ms = 100;
+
+    /* config[BUTTON_ID_UP]->click.handler = (ClickHandler) select_single_click_handler; */
+    /* config[BUTTON_ID_UP]->click.repeat_interval_ms = 100; */
+
+    config[BUTTON_ID_DOWN]->click.handler = (ClickHandler) next_track;
+    config[BUTTON_ID_DOWN]->click.repeat_interval_ms = 100;
+}
+
+void http_success_callback(int32_t cookie, int http_status, DictionaryIterator* response, void* context) {
+    // Do nothing.
 }
 
 void handle_init(AppContextRef ctx) {
